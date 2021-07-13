2070958c605165b600d7876232300b230781d5f2
AntonioBMN <toinho.neto@aluno.uece.br>
2021-07-13 07:51:45 -0300

    2. Tokens transferidos

--- contracts/PudimToken.sol
+++ contracts/PudimToken.sol
@@ -3,15 +3,27 @@ pragma solidity ^0.5.16;
 contract PudimToken {
 
     string public name =  "Pudim Token";
-
+    string public symbol =  "PDT"; 
+    string public standard =  "PDT Token v1.0";     
     uint256 public totalSupply;
 
+    event Transfer(
+        address indexed _from,
+        address indexed  _to, 
+        uint256 _value);
     mapping(address => uint) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
-
-
     }
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
 }
--- test/PudimToken.js
+++ test/PudimToken.js
@@ -9,8 +9,11 @@ contract('PudimToken', function(accounts){
             assert.equal(name, 'Pudim Token', 'Tem o nome correto');
             return tokenInstance.symbol();
         }).then(function(symbol){
-            assert.equal(symbol, 'PDT', 'Tem o token correto');
-        });
+            assert.equal(symbol, 'PDT', 'Tem o simbolo correto');
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, 'PDT Token v1.0', 'Tem o padrão correto')
+        })
     });
     it('Aloca o supplyInicial',function(){
         return PudimToken.deployed().then(function(instance){
@@ -21,6 +24,30 @@ contract('PudimToken', function(accounts){
             return tokenInstance.balanceOf(accounts[0]);
         }).then(function(adminBalance){
             assert.equal(adminBalance.toNumber(), 1000000, "Aloca o initial supply para conta adm");
+        })
+    })
+    it('Transferiu o token do dono', function(){
+        return PudimToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1],999999999);
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'error message must contain revert');
+            return tokenInstance.transfer.call(accounts[1],250000, {from: accounts[0] });
+        }).then(function(success){
+            assert.equal(success, true, 'Retorna true')
+            return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length,1, 'ativa um evento');
+            assert.equal(receipt.logs[0].event, 'Transfer', '"Transfer" event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'Conta que envia');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'conta que recebe');
+            assert.equal(receipt.logs[0].args._value, 250000, 'valor a ser transferido');
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(),250000, 'Adiciona a quantidade a conta que recebe');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(),750000, 'Diminui a quantidade da conta que doa');
         })
     })
 })
